222f70ed3600f74a4738ecaa700ae479a59d69b7
EmilioDeChiara3388 <emilio.dechiara3388@gmail.com>
2025-02-12 11:38:20 +0100

    add: snack 1 part 2

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
 // Crea una funzione dichiarativa chiamata somma che accetta due numeri e restituisce la loro somma.
 
-function somma(num1, num2) {
+/* function somma(num1, num2) {
     return num1 + num2;
 }
 
 console.log(somma(3, 4));
-console.log(somma(2, 5));
+console.log(somma(2, 5)); */
+
+// Poi, definisci la stessa funzione somma ma come funzione anonima assegnata a una variabile
+
+const somma = function (num1, num2) {
+    return num1 + num2;
+}
+
+console.log(somma(1, 3));
+
+
+
+
 
