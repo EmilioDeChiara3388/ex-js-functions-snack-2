1888f49bbb277f968f16b640b43d39c2cb8b2b88
EmilioDeChiara3388 <emilio.dechiara3388@gmail.com>
2025-02-12 11:33:45 +0100

    add: first snack

---
+++ main.js
@@ -0,0 +1,9 @@
+// Crea una funzione dichiarativa chiamata somma che accetta due numeri e restituisce la loro somma.
+
+function somma(num1, num2) {
+    return num1 + num2;
+}
+
+console.log(somma(3, 4));
+console.log(somma(2, 5));
+
