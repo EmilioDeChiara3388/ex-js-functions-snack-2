6e88952da50d288870dbe21f6abbffa89414025f
EmilioDeChiara3388 <emilio.dechiara3388@gmail.com>
2025-02-12 11:40:41 +0100

    add: snack 1 part 3

--- main.js
+++ main.js
@@ -9,11 +9,18 @@ console.log(somma(2, 5)); */
 
 // Poi, definisci la stessa funzione somma ma come funzione anonima assegnata a una variabile
 
-const somma = function (num1, num2) {
+/* const somma = function (num1, num2) {
     return num1 + num2;
 }
 
-console.log(somma(1, 3));
+console.log(somma(1, 3)); */
+
+// Quindi, riscrivi la funzione somma con la sintassi delle arrow functions.
+
+const somma = (num1, num2) => console.log(num1 + num2);
+
+somma(6, 9);
+
 
 
 
